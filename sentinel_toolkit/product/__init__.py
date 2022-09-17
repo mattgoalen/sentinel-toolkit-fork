556e30bec1b09e7baaf0a12c8e4e528d3a771463
gdgenchev <gdgenchev97@gmail.com>
2022-09-17 21:55:25 +0300

    Add product metadata xml wrapper (#5)

---
+++ sentinel_toolkit/product/__init__.py
@@ -0,0 +1,10 @@
+"""
+Product
+================
+
+Product module provides tools for working with a Sentinel-L2A product.
+
+It provides the class ProductMetadata that has methods for retrieving some
+metadata information (Sensing Date, NODATA Value, Quantification Value,
+BOA Offset Values, Solar Irradiances) from the product's MTD_MSIL2A.xml.
+"""
